1e1c457af5c287905ac370ebcffac277861552c7
gothicvoid <774377561@qq.com>
2017-05-07 21:01:20 +0800

    新增实体类News和NewsTags，分别描述新闻和标签； 增加初始化类Initializer，用于初始化程序（未完成）； 拓展NewsReader，增加获取标题、标签、日期等内容的功能。

---
+++ src/util/Initializer.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.ArrayList;
+
+import model.News;
+
+/*
+ * 用于初始化整个程序及保存修改。
+ * 程序加载时读取xml文件，并把相关内容存进News类；
+ * 程序结束时把对新闻的修改（主要是标签的修改和新闻的“删除”）存回文件。
+ */
+
+public class Initializer {
+
+	public ArrayList<News> newsList;
+	
+	/*
+	 * 初始化代码块（待实现）
+	 */
+	
+	public void saveChanges(){
+		/*
+		 * 保存修改到文件（待实现）
+		 */
+	}
+	
+}
