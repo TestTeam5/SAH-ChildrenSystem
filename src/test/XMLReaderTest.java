5139539faa12ad82e02594fd08af88caaa37d0b1
Aglaia95 <578657718@qq.com>
2017-05-08 23:48:01 +0800

    xmlreader测试

---
+++ src/test/XMLReaderTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import util.XMLReader;
+
+
+public class XMLReaderTest {
+    private XMLReader xmlreader = new XMLReader("resource/file/xuexiao.xml");
+    
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+
+
+	@Test
+	public void testXMLReader() throws Exception {
+	}
+
+	@Test
+	public void ReadXml() throws Exception {
+		ArrayList<Map<String, String>> list = xmlreader.readXml();
+		for(Map<String, String> item : list){
+		    assertEquals("青少年“身边最让我感动的人”评选揭晓", item.get("Title"));
+			assertEquals("2006-01-16", item.get("Date"));
+			assertEquals("光明日报", item.get("Location"));
+			assertEquals("02,教科文衞", item.get("Type"));
+			assertEquals("399", item.get("WordCount"));
+			assertEquals("news:23lh^200601161410077(S:193916305)", item.get("ID"));
+			assertEquals("确保农民工子女享受国家政策", item.get("Title"));
+			assertEquals("2006-03-04", item.get("Date"));
+			assertEquals("光明日报", item.get("Location"));
+			assertEquals("06,两会特刊", item.get("Type"));
+			assertEquals("724", item.get("WordCount"));
+			assertEquals("news:12bc^200603041410010(S:193916305)", item.get("ID"));
+		}
+		
+	}
+
+}
