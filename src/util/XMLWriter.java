f3d13dc1cb08f1251d0d0d35362796cf26fc85bd
gothicvoid <774377561@qq.com>
2017-05-14 23:21:07 +0800

    完成标签保存功能

---
+++ src/util/XMLWriter.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.io.FileOutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLWriter {
+	
+	public void write(String path, String ID, String tagName, String tagValue){
+
+        try {
+            // 1.得到DOM解析器的工厂实例
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            // 2.从DOM工厂里获取DOM解析器
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // 3.解析XML文档，得到document，即DOM树
+            Document doc = db.parse(path);
+            NodeList list = doc.getElementsByTagName("ID");
+            
+            for(int i = 0; i < list.getLength(); i++){
+            	Node node = list.item(i);
+                if(node.getFirstChild().getNodeValue().equals(ID)){
+                	Node parentNode = node.getParentNode();
+					NodeList nChildList = parentNode.getChildNodes();
+					for (int j = 0; j < nChildList.getLength(); j++) {
+						if (Node.ELEMENT_NODE == nChildList.item(j).getNodeType()) {
+							Node childNode = nChildList.item(j);
+							if(childNode.getNodeName().equals(tagName)){
+								if(!childNode.hasChildNodes()){
+									childNode.appendChild(doc.createTextNode(tagValue));
+								}else{
+									childNode.getFirstChild().setNodeValue(tagValue);
+								}
+							}
+						}
+					}
+					break;
+                }
+            }
+            
+            //保存xml文件
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(doc);
+            //设置编码类型
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            StreamResult result = new StreamResult(new FileOutputStream(path));
+            //把DOM树转换为xml文件
+            transformer.transform(domSource, result);
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
